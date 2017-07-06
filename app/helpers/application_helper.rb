9e80a5e18a72dc5bd0eb9f76a680e87ba910c8c9
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-07-05 23:02:27 -0700

    Added markdown to wiki creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,26 @@ module ApplicationHelper
     # => used to build the html and css to dosplay the form element and any associated errors
     content_tag :div, capture(&block), class: css_class
   end
+
+  def markdown(text)
+       options = {
+           hard_wrap: true,
+           filter_html: true,
+           no_intra_emphasis: true,
+           fenced_code_blocks: true,
+           tables: true,
+           lax_html_blocks: true,
+           strikethrough: true,
+       }
+
+       extensions = {
+       	autolink: true,
+       	superscript: true,
+       	disable_indented_code_blocks: true
+       }
+       renderer = Redcarpet::Render::HTML.new(options)
+       markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+       markdown.render(text).html_safe
+   end
 end
