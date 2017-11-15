86ea46251bc262f9ae39ed44c217116ac9433d15
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-11-14 21:25:30 -0800

    added readme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,15 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+var blocmetrics = {};
+	blocmetrics.report = function(upload) {
+		
+	var event = {event: { name: "upload" }};
+
+   var request = new XMLHttpRequest();
+ 	request.open("POST", "http://localhost:3000/api/events", true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify(event));
+
+	};
